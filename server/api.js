60da75322ec723aa897dce0be30db70bc306c409
m-grigoriu <marcgrigoriu2003@gmail.com>
2023-10-16 22:37:00 -0400

    added insertion queries to api, modified form data.

--- server/api.js
+++ server/api.js
@@ -9,9 +9,84 @@ app.listen(3300, ()=>{
 client.connect();
 
 app.get('/users', (req, res)=>{
+    client.query(`Select * from LoginInfo`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+app.get('/students', (req, res)=>{
     client.query(`Select * from Student`, (err, result)=>{
         if(!err){
             res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+app.get('/friendpreferences', (req, res)=>{
+    client.query(`Select * from FriendPreference`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+app.get('/rooms', (req, res)=>{
+    client.query(`Select * from Room`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
+})
+
+//INSERTIONS START HERE
+app.post('/users', (req, res)=>{
+    const user = req.body;
+    let insertUserQuery = `INSERT INTO LoginInfo(username, password) values(${user.username}, '${user.password}')`
+    client.query(insertUserQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion Worked');
+        }
+    });
+    client.end;
+})
+
+app.post('/students', (req, res)=>{
+    const student = req.body;
+    let insertStudentQuery = `INSERT INTO Student(sID, firstName, lastName, grade, genderType, groupID) 
+    values(${student.SID}, '${student.first_name}', '${student.last_name}', '${student.grade}', '${student.gender}', null)`
+    client.query(insertStudentQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion Worked');
+        }
+    });
+    client.end;
+})
+
+app.post('/friendpreferences', (req, res)=>{
+    const preference = req.body;
+    let insertPreferenceQuery = `INSERT INTO FriendPreference(SID1, SID2, isApart) 
+    values(${preference.first_pref_SID}, '${preference.second_pref_SID}', '${preference.relationship}')`
+    client.query(insertPreferenceQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion Worked');
+        }
+    });
+    client.end;
+})
+
+app.post('/rooms', (req, res)=>{
+    const room = req.body;
+    let insertRoomQuery = `INSERT INTO Room(rID, roomType) 
+    values(${room.room_num}, '${room.room_type}')`
+    client.query(insertRoomQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion Worked');
         }
     });
     client.end;
