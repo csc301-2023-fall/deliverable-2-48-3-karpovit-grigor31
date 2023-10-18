1347d048bf54971a7a7b898093689d14618cbf28
m-grigoriu <marcgrigoriu2003@gmail.com>
2023-10-18 01:07:11 -0400

    Added display for data on webpage, js code to prevent redirects on http response and instead display information on web page dynamically.

--- server/api.js
+++ server/api.js
@@ -4,6 +4,7 @@ const express = require('express'),
       app = express();
 
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
@@ -22,6 +23,8 @@ app.get('/users', (req, res)=>{
     client.query(`Select * from LoginInfo`, (err, result)=>{
         if(!err){
             res.send(result.rows);
+        } else {
+            res.send('Failed to retrieve data.')
         }
     });
     client.end();
@@ -31,6 +34,8 @@ app.get('/students', (req, res)=>{
     client.query(`Select * from Student`, (err, result)=>{
         if(!err){
             res.send(result.rows);
+        } else {
+            res.send('Failed to retrieve data.')
         }
     });
     client.end();
@@ -40,6 +45,8 @@ app.get('/friendpreferences', (req, res)=>{
     client.query(`Select * from FriendPreference`, (err, result)=>{
         if(!err){
             res.send(result.rows);
+        } else {
+            res.send('Failed to retrieve data.')
         }
     });
     client.end;
@@ -49,6 +56,8 @@ app.get('/rooms', (req, res)=>{
     client.query(`Select * from Room`, (err, result)=>{
         if(!err){
             res.send(result.rows);
+        } else {
+            res.send('Failed to retrieve data.')
         }
     });
     client.end;
@@ -58,6 +67,8 @@ app.get('/counselors', (req, res)=>{
     client.query(`Select * from Counselor`, (err, result)=>{
         if(!err){
             res.send(result.rows);
+        } else {
+            res.send('Failed to retrieve data.')
         }
     });
     client.end;
@@ -69,9 +80,9 @@ app.post('/users', (req, res)=>{
     let insertUserQuery = `INSERT INTO LoginInfo(username, password) values('${user.username}', '${user.password}')`
     client.query(insertUserQuery, (err, result)=>{
         if(!err){
-            res.send('Insertion Worked');
+            res.send('Inserted user!');
         } else {
-            res.send('Failed')
+            res.send('Failed to insert data.')
         }
     });
     client.end;
@@ -83,7 +94,9 @@ app.post('/students', (req, res)=>{
     values('${student.SID}', '${student.first_name}', '${student.last_name}', '${student.grade}', '${student.gender}', null)`
     client.query(insertStudentQuery, (err, result)=>{
         if(!err){
-            res.send('Insertion Worked');
+            res.send('Inserted student!');
+        } else {
+            res.send('Failed to insert data.')
         }
     });
     client.end;
@@ -95,7 +108,9 @@ app.post('/friendpreferences', (req, res)=>{
     values('${preference.first_pref_SID}', '${preference.second_pref_SID}', '${preference.relationship}')`
     client.query(insertPreferenceQuery, (err, result)=>{
         if(!err){
-            res.send('Insertion Worked');
+            res.send('Inserted preference!');
+        } else {
+            res.send('Failed to insert data.')
         }
     });
     client.end;
@@ -107,7 +122,9 @@ app.post('/rooms', (req, res)=>{
     values('${room.room_num}', '${room.room_type}')`
     client.query(insertRoomQuery, (err, result)=>{
         if(!err){
-            res.send('Insertion Worked');
+            res.send('Inserted room!');
+        } else {
+            res.send('Failed to insert data.')
         }
     });
     client.end;
@@ -119,7 +136,9 @@ app.post('/counselors', (req, res)=>{
     values('${counselor.counselor_firstname}', '${counselor.counselor_lastname}', null)`
     client.query(insertCounselorQuery, (err, result)=>{
         if(!err){
-            res.send('Insertion Worked');
+            res.send('Inserted counselor!');
+        } else {
+            res.send('Failed to insert data.')
         }
     });
     client.end;
---
+++ summer_camp_data.js
@@ -0,0 +1,297 @@
+//Display retrieved data from HTTP response in the 'returned data' element
+function displayData(data) {
+    document.getElementById("returnedData").innerHTML = data;
+}
+
+//Insertion functions start here
+//Insert user and display success/failure message
+//General format for this function/event handler based on code from https://www.javascript-coder.com/javascript-form/javascript-submit-form-post-method/
+function submitUsers(e) {
+  e.preventDefault(); //Prevent default html submit form action from happening
+   
+  var userForm = document.getElementById("user-input");
+  var formData = new FormData(userForm); //Retrieve inputted form data
+  
+  fetch("http://localhost:3300/users", { //Create POST request with inputted data
+    method: "POST",
+    body: formData,
+  })  
+    
+  .then(response => { //Upon receiving response, turn it into a string and call displayData
+    if (!response.ok) {
+      throw new Error('RESPONSE ERROR');
+    }
+    return response.json();
+  })
+    
+  .then((response) => {
+    displayData(JSON.stringify(response, null, 1));
+  })
+    
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+}
+  
+var userForm = document.getElementById("user-input"); //Add this function with an eventlistener on the form element
+userForm.addEventListener("submit", submitUsers);
+
+//Insert student and display success/failure message
+function submitStudents(e) {
+  e.preventDefault();
+   
+  var studentForm = document.getElementById("student-input");
+  var formData = new FormData(studentForm);
+  
+  fetch("http://localhost:3300/students", {
+    method: "POST",
+    body: formData,
+  })
+      
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('RESPONSE ERROR');
+    }
+    return response.json();
+  })
+      
+  .then((response) => {
+    displayData(JSON.stringify(response, null, 1));
+  })
+      
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+}
+  
+var studentForm = document.getElementById("student-input");
+studentForm.addEventListener("submit", submitStudents);
+
+//Insert student preference and display success/failure message
+function submitPreferences(e) {
+  e.preventDefault();
+   
+  var preferenceForm = document.getElementById("student-preference");
+  var formData = new FormData(preferenceForm);
+  
+  fetch("http://localhost:3300/friendpreferences", {
+    method: "POST",
+    body: formData,
+  })
+    
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('RESPONSE ERROR');
+    }
+    return response.json();
+  })
+      
+  .then((response) => {
+    displayData(JSON.stringify(response, null, 1));
+  })
+      
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+}
+  
+var preferenceForm = document.getElementById("student-preference");
+preferenceForm.addEventListener("submit", submitPreferences);
+
+//Insert room and display success/failure message
+function submitRooms(e) {
+  e.preventDefault();
+   
+  var roomForm = document.getElementById("room-input");
+  var formData = new FormData(roomForm);
+  
+  fetch("http://localhost:3300/rooms", {
+    method: "POST",
+    body: formData,
+  })
+      
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('RESPONSE ERROR');
+    }
+    return response.json();
+  })
+      
+  .then((response) => {
+    displayData(JSON.stringify(response, null, 1));
+  })
+      
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+}
+  
+var roomForm = document.getElementById("room-input");
+roomForm.addEventListener("submit", submitRooms);
+
+//Insert counselor and display success/failure message
+function submitCounselors(e) {
+  e.preventDefault();
+   
+  var counselorForm = document.getElementById("counselor-input");
+  var formData = new FormData(counselorForm);
+  
+  fetch("http://localhost:3300/counselors", {
+    method: "POST",
+    body: formData,
+  })
+      
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('RESPONSE ERROR');
+    }
+    return response.json();
+  })
+      
+  .then((response) => {
+    displayData(JSON.stringify(response, null, 1));
+  })
+      
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+}
+  
+var counselorForm = document.getElementById("counselor-input");
+counselorForm.addEventListener("submit", submitCounselors);
+
+//Select functions start here
+//Retrieve stored user data and display it or failure message
+function getUsers(e) {
+  e.preventDefault();
+  
+  fetch("http://localhost:3300/users", { //Get requests are fetched with no body
+    method: "GET"
+  })
+      
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('RESPONSE ERROR');
+    }
+    return response.json();
+  })
+      
+  .then((response) => {
+    displayData(JSON.stringify(response, null, 1));
+  })
+      
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+}
+  
+var usersRetrieve = document.getElementById("GetUsers");
+usersRetrieve.addEventListener("submit", getUsers);
+
+//Retrieve stored student data and display it or failure message
+function getStudents(e) {
+  e.preventDefault();
+   
+  fetch("http://localhost:3300/students", {
+    method: "GET"
+  })
+      
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('RESPONSE ERROR');
+    }
+    return response.json();
+  })
+      
+  .then((response) => {
+    displayData(JSON.stringify(response, null, 1));
+  })
+      
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+}
+  
+var studentsRetrieve = document.getElementById("GetStudents");
+studentsRetrieve.addEventListener("submit", getStudents);
+
+//Retrieve stored student preference data and display it or failure message
+function getPreferences(e) {
+  e.preventDefault();
+  
+  fetch("http://localhost:3300/friendpreferences", {
+    method: "GET"
+  })
+      
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('RESPONSE ERROR');
+    }
+    return response.json();
+  })
+      
+  .then((response) => {
+    displayData(JSON.stringify(response, null, 1));
+  })
+      
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+}
+  
+var preferencesRetrieve = document.getElementById("GetPreferences");
+preferencesRetrieve.addEventListener("submit", getPreferences);
+
+//Retrieve stored room data and display it or failure message
+function getRooms(e) {
+  e.preventDefault();
+  
+  fetch("http://localhost:3300/rooms", {
+    method: "GET"
+  })
+      
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('RESPONSE ERROR');
+    }
+    return response.json();
+  })
+      
+  .then((response) => {
+    displayData(JSON.stringify(response, null, 1));
+  })
+      
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+}
+  
+var roomsRetrieve = document.getElementById("GetRooms");
+roomsRetrieve.addEventListener("submit", getRooms);
+
+//Retrieve stored counselor data and display it or failure message
+function getCouncelors(e) {
+  e.preventDefault();
+   
+  fetch("http://localhost:3300/councelors", {
+    method: "GET"
+  })
+      
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('RESPONSE ERROR');
+    }
+    return response.json();
+  })
+      
+  .then((response) => {
+    displayData(JSON.stringify(response, null, 1));
+  })
+      
+  .catch((err) => {
+    console.log("Error: ", err);
+  });
+}
+  
+var councelorsRetrieve = document.getElementById("GetCounelors");
+councelorsRetrieve.addEventListener("submit", getCouncelors);
