e255f8a4fd45fa9ef744f1dbccc937ba99ac7a06
Eric Karpovits <erickarpovits@gmail.com>
2023-10-19 00:13:53 -0400

    ADDING CORS handling to ec2 server

--- server/api.js
+++ server/api.js
@@ -5,6 +5,13 @@ const express = require('express'),
 
 const bodyParser = require("body-parser");
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
